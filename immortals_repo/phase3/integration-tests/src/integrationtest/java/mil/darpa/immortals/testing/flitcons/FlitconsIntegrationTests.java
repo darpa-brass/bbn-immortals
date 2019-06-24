6c6d0f42980e003b39e6d81e23dd07ebe3700031
Austin Wellman <austin.wellman@raytheon.com>
2019-06-24 11:40:21 -0400

    Misc Fixes and documentation updates

--- immortals_repo/phase3/integration-tests/src/integrationtest/java/mil/darpa/immortals/testing/flitcons/FlitconsIntegrationTests.java
+++ immortals_repo/phase3/integration-tests/src/integrationtest/java/mil/darpa/immortals/testing/flitcons/FlitconsIntegrationTests.java
@@ -16,22 +16,18 @@ public class FlitconsIntegrationTests {
 //	}
 
 	@Test
-	public void basicTest() {
+	public void testServerReady() {
 		JarTestScenarioRunner.createScenario5Runner("s5").execute();
 	}
+	
+	@Test
+	public void testServerWaitForReady() {
+		JarTestScenarioRunner.createScenario5Runner("s5").execute(4000);
+	}
 
-//	@Test
-//	public void testServerWaitForReady() {
-//		runTest(TestScenario.getTestScenario("s5"), false, 4000);
-//	}
 
-//	@Test
-//	public void testServerReady() {
-//		runTest(TestScenario.getTestScenario("s5"), false, null);
-//	}
-
-//	@Test
-//	public void testServerSequential() {
-//		JarTestScenarioRunner.createScenario5Runner("s5s").execute();
-//	}
+	@Test
+	public void testServerSequential() {
+		JarTestScenarioRunner.createScenario5Runner("s5s").execute();
+	}
 }
